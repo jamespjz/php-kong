5b52dc25ca1e9aa8a5e1ed9801a6bc2d99203144
pijianzhong <zt13536@zongteng.net>
2020-01-23 13:22:02 +0800

    add test

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -3,12 +3,20 @@
  * 操作类
  */
 
-namespace Jamespi\Kong\Controller;
+namespace Jamespi\Kong\Controllers;
 
+use Jamespi\Kong\Common\HttpHelper;
 class IndexController{
 
     public function index(){
-        echo 111;
+        $http = new HttpHelper();
+        $baseUri = 'http://47.112.160.146:8001';
+        $body = [
+            'name' => 'james_service',
+            'url' => 'http://music.taihe.com'
+        ];
+        $apiStr = '/services/';
+        $http->Post($baseUri, $body, $apiStr);
     }
 
 }
